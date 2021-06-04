3a0c596ab655e53eda50d9e12cc2001ed68b6ffe
Aleksey Efimov <factorng@yandex.ru>
2021-06-04 19:41:08 +0300

    README.md added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import Join from "./components/Join";
 import Chat from "./components/Chat";
 import JoinByLink from "./components/JoinByLink";
@@ -9,10 +9,11 @@ import "./App.css";
 export default function App() {
   return (
     <Router history={history}>
-      <Route path="/" exact component={Join} />
-      <Route path="/join" exact component={Join} />
-      <Route path="/joinByLink" component={JoinByLink} />
-      <Route path="/chat" component={Chat} />
+      <Switch>
+        <Route path="/" exact component={Join} />
+        <Route path="/joinByLink" component={JoinByLink} />
+        <Route path="/chat" component={Chat} />
+      </Switch>
     </Router>
   );
 }
